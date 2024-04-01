bc16f2c29ecb3ceb6220690f6473507ddb9bfc30
Ankush Makkar <ankushmakkar108@gmail.com>
2024-04-01 22:25:52 +0530

    bug fix and title component

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,38 +2,21 @@ import React from "react";
 import { useSelector } from "react-redux";
 import VidContainer from "./VidContainer";
 import Loading from "./Loading";
-import { useNavigate } from "react-router-dom";
+import MainTitle from "./MainTitle";
 
 const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
-  const navigate = useNavigate();
   if (!movies) return <Loading />;
 
   const MainMovie = movies[Math.floor(Math.random() * 20)];
-  const { title, overview, id } = MainMovie;
+  const { id } = MainMovie;
 
   return (
     <div className="relative ">
-      {/* Main Bg video container */}
       <div className=" bg-black overflow-hidden max-h-screen">
         <VidContainer id={id} />
       </div>
-      <div className="w-screen aspect-video absolute top-0 text-white bg-gradient-to-tr from-black pl-12 min-h-[500px] max-h-screen ">
-        <div className="absolute lg:bottom-48 bottom-40">
-          <h1 className="sm:text-4xl pb-2 text-lg font-semibold">{title}</h1>
-          <p className="sm:text-lg text-sm lg:w-1/2 md:w-1/2 xs:w-3/4 w-5/6 ">
-            {overview.split(" ").splice(0, 19).join(" ")}...
-          </p>
-          <div className="flex w-fit py-2">
-            <button
-              onClick={() => navigate("/movie?id=" + id)}
-              className="p-2 px-6 m-1 bg-white bg-opacity-50 font-bold md:text-xl sm:text-md text-sm  rounded-md text-black z-40 hover:bg-red-600 hover:bg-opacity-70  hover:text-white duration-150"
-            >
-              More Info
-            </button>
-          </div>
-        </div>
-      </div>
+      <MainTitle id={id} />
     </div>
   );
 };
